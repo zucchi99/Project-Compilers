7a318ab2ae6f8a7a76f64f67e1169d91bcff04b7
root <riccardo.lunardi1999@gmail.com>
2022-09-23 09:34:17 +0000

    commentini

--- part_2/prova.cpp
+++ part_2/prova.cpp
@@ -10,7 +10,7 @@ int main() {
     auto assignment4 = std::pair<Value, std::list<Value*>>(Value("$sezione2.c", NULL), temp_empty_list);
     auto assignment5 = std::pair<Value, std::list<Value*>>(Value("$sezione1.a", NULL), temp_empty_list);
     auto assignment6 = std::pair<Value, std::list<Value*>>(Value("$sezione1.d", NULL), temp_empty_list);
-    auto assignment7 = std::pair<Value, std::list<Value*>>(Value("$sezione1.e", NULL), temp_empty_list);
+    auto assignment7 = std::pair<Value, std::list<Value*>>(Value("$e", NULL), temp_empty_list);
 
    std::map<std::string, std::pair<Value, std::list<Value*>>> assignments = {{ "a", assignment1 }, { "b", assignment2 }, { "c", assignment3 }, { "d", assignment4 }, { "e", assignment6 }, { "f", assignment7 }};
     
@@ -28,7 +28,7 @@ int main() {
 
     // std::cout << s << std::endl;
 
-    s.delete_section("sezione1");
+    // s.delete_section("sezione1");
 
     std::cout << s << std::endl;
 
@@ -36,14 +36,16 @@ int main() {
     // s.delete_assignment("sezione1", "d");
     // std::cout << "Caso 1, sezione1 d " << std::endl << s << std::endl;
     // // Caso2
-    // s.delete_assignment("sezione1", "a");
-    // std::cout << "Caso 2, sezione1 a " << std::endl << s << std::endl;
-    // // Caso3
+    //s.delete_assignment("sezione1", "a");
+    //std::cout << "Caso 2, sezione1 a " << std::endl << s << std::endl;
+    // Caso3
     // s.delete_assignment("sezione1", "e");
     // std::cout << "Caso 3, sezione1 e " << std::endl << s << std::endl;
     // // Caso4
     // s.delete_assignment("sezione1", "b");
     // std::cout << "Caso 4, sezione1 b " << std::endl << s << std::endl;
+
+    std::cout << s.to_String(true) << std::endl;
 
     return 0;
 }
