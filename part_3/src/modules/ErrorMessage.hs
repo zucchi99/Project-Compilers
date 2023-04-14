276f94d3539dfbe2050495d8f90d33606f51f591
dbrglc <gianluca.dabrosca.1999@gmail.com>
2023-04-14 10:09:47 +0000

    Add modification for Static semantic

--- part_3/src/modules/ErrorMessage.hs
+++ part_3/src/modules/ErrorMessage.hs
@@ -2,10 +2,16 @@
 
 module ErrorMessage where
 
+errMsgNotImplemented :: (Show a) => a -> (Int, Int) -> [Char]
+errMsgNotImplemented s pos = "In the block " ++ printPos pos ++ (show s) ++ " is forward declared but not implemented"
+
+errMsgWrongFunctionType :: (Show a) => a -> (Int, Int) -> [Char]
+errMsgWrongFunctionType s pos = printPos pos ++ (show s) ++ " is forward declared but the types of the parameters and/or the return are different"
+
 errMsgAssign :: (Show a1, Show a2) => a2 -> a1 -> (Int, Int) -> [Char]
 errMsgAssign lhs rhs pos = printPos pos ++ "Can't assign value of type " ++ (show rhs) ++ " to variable of type " ++ (show lhs)
 
-errMsgNotMathType :: Show a => a -> (Int, Int) -> [Char]
+errMsgNotMathType :: (Show a) => a -> (Int, Int) -> [Char]
 errMsgNotMathType t pos = printPos pos ++ (show t) ++ "Not a math type"
 
 errMsgNotCompatible :: (Show a1, Show a2) => a1 -> a2 -> (Int, Int) -> [Char]
@@ -29,7 +35,7 @@ errMsgNotDeclared id pos = printPos pos ++ "The name " ++ (show id) ++ " is not 
 errMsgUnexpectedType :: (Show a1, Show a2) => [Char] -> a1 -> a2 -> (Int, Int) -> [Char]
 errMsgUnexpectedType obj t_exp t_found pos = printPos pos ++ obj ++ " must be of type " ++ (show t_exp)  ++ " but type " ++ (show t_found) ++ " is given"
 
-errMsgTypeNotArray :: Show a => a -> (Int, Int) -> [Char]
+errMsgTypeNotArray :: (Show a) => a -> (Int, Int) -> [Char]
 errMsgTypeNotArray t_found pos = printPos pos ++ "Expected an array but type " ++ (show t_found) ++ " is given"
 
 errMsgClash :: (Show a) => a -> (Int, Int) -> [Char]
