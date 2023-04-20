c8b37be9accf85c42ac1d430df850b07d961c693
zucchi99 <francesco.zuccato99@gmail.com>
2023-04-21 00:48:34 +0200

    .

--- part_3/src/test_files/test_TAC.pas
+++ part_3/src/test_files/test_TAC.pas
@@ -1,16 +1,27 @@
 program TAC;
-
+var
+   x : boolean;
+   y : boolean;
+   z : integer;
+   
 begin
-   if 3 < 2 * 1 / 4 or 0 < 3 
+   if 1 > 0
    then 
-   //begin
-      if 3 < 2 * 1 / 4 or 0 < 3 
+   begin
+      if 2 = 0
       then
+      begin
          x := true or false;
-   //end;
-   y := 1 > 2 or 2 > 1;
+      end
+      else 
+         //begin
+            y := true or false;
+         //end
+   end;
+   //y := 1 > 2 or 2 > 1;
    //if 3 < 2 * 1 / 4 or 0 < 3 then print(3);
    //x := 3 > 0; // or 5 < 4;
+   z := 2;
 end.
 
 
