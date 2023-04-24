069213f2edba7badd976d51f12c9e79b6c60ea8e
Riccardo Lunardi <riccardo.lunardi1999@gmail.com>
2023-04-24 16:12:10 +0200

    while

--- part_3/src/test_files/test_while.pas
+++ part_3/src/test_files/test_while.pas
@@ -1,13 +1,17 @@
 program whileLoop;
 var
    a: integer;
-
 begin
    a := 10;
    while  a < 20  do
-   
    begin
-      writeln("value of a: ", a);
       a := a + 1;
+      if a = 15 then
+      begin
+         a := a + 4;
+         continue;
+      end;
+      a := a + 2;
    end;
+   a := a + 3;
 end.
