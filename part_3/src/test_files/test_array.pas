6ba6edaec18bf8b8de2dc1bf4ec9ea07d108c5fd
zucchi99 <francesco.zuccato99@gmail.com>
2023-04-24 13:46:10 +0200

    .

--- part_3/src/test_files/test_array.pas
+++ part_3/src/test_files/test_array.pas
@@ -1,14 +1,17 @@
 program exArrays;
 var
-   n: array [1..10, 2..1] of integer;   (* n is an array of 10 integers *)
+   a : array [1..10, 1..1] of integer;
+   z : array [3..5, 4..6] of integer;
+   x : array [1..3, 2..4] of array [3..5, 4..6] of integer ;   (* n is an array of 10 integers *)
    i, j: integer;
+   p : ^integer;
 
 begin
-   (* initialize elements of array n to 0 *)        
+
+   x[1,1] := z;
+   // initialize elements of array n to 0
    for i := 1 to 10 do
-       n[ i ] := i + 100;   (* set element at location i to i + 100 *)
-    (* output each array element's value *)
-   
-   for j:= 1 to 10 do
-      writeln("Element[", j, "] = ", n[j] );
+      a[ i, 1 ] := i + 100;   (* set element at location i to i + 100 *)
+      //output each array element's value       
+      writeInt(a[i,1])
 end.
