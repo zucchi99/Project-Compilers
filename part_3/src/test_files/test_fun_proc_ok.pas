af8074a6434de2b0198b7cc6286628b151a41788
Riccardo Lunardi <riccardo.lunardi1999@gmail.com>
2023-04-29 00:18:58 +0200

    test più completo

--- part_3/src/test_files/test_fun_proc_ok.pas
+++ part_3/src/test_files/test_fun_proc_ok.pas
@@ -2,6 +2,7 @@ program Hello;
 
 var 
 x : integer;
+q : char;
 a : string;
 z : array [1..10] of string;
 
@@ -14,16 +15,16 @@ begin
         a := 1 + 2;
         p := "ciao";
         c := 'c';
+        q := 'q';
     end;
 
-    (*procedure g(); 
-    begin 
-    end;
-    *)
+    procedure g(a,x : integer); begin end;
+    
     begin 
         //x := 0;
         //g();
-        z[1+2] := p(x,x,z[1]);
+        z[1+2] := p(q,x,z[1]);
+        g(q,x);
         //x := p();
     end;
     //a := p();
