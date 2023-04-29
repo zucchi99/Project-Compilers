d5436265d3ba2cf9f6a6afb42537c36e9df6ffbf
zucchi99 <francesco.zuccato99@gmail.com>
2023-04-29 11:01:00 +0200

    .

--- part_3/src/test_files/test_fun_param.pas
+++ part_3/src/test_files/test_fun_param.pas
@@ -1,12 +1,19 @@
 program ciao;
 
 procedure p (a:integer ; var b: integer ; result c:integer);
+    function g() : integer; 
+    begin
+        g := 3;
+    end;
+var
+    p : integer;
 begin
+    p := 2;
 end;
 
 var
     d: integer;
 
 begin
-    p(d,d@,d);
+    p(d,d@,d@);
 end.
