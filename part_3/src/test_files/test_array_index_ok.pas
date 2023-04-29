0836bd5d14646e8785c8e0ec5c97cd4cf5e00c0f
zucchi99 <francesco.zuccato99@gmail.com>
2023-04-29 18:38:23 +0200

    .

---
+++ part_3/src/test_files/test_array_index_ok.pas
@@ -0,0 +1,33 @@
+program exArrays;
+
+var
+   x :  array [0..3,0..1] of array [0..2] of integer ;
+   //p : ^array [0..3,0..1] of array [0..2] of integer ;
+   q : ^array [0..2] of integer ;
+   //i,j,k : integer;
+
+begin
+
+   //writeInt(x[1,2][0])
+
+   // no copy di array
+   //x := z;
+
+   // use pointer of sub-array
+   q := x[1,2]@;
+
+   // use pointer of whole array
+   (*p := x@;
+
+   // initialize multi dimensional array
+   for i := 0 to 3 do
+      for j := 0 to 1 do
+         for k := 0 to 2 do
+         begin
+            //x[i,j][k] := i + j + k;
+            p^[i,j][k] := i + j + k;
+            //writeInt(x[i,j][k])
+         end;
+   *)
+
+end.
